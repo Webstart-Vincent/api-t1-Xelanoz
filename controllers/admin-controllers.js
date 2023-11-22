4f65426d4cc139851cac0bdc3aa9ada504864e78
Xelanoz <dias.taichi@gmail.com>
2023-11-22 15:49:55 +0100

    first commit

---
+++ controllers/admin-controllers.js
@@ -0,0 +1,101 @@
+const User = require('../model/user');
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const mailPattern = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+const passwordPattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,}$/;
+
+module.exports = {
+    checkAuthPayload: (req, res, next) => {
+        const { email, password } = req.body;
+
+        if (!email || !password) {
+            return res.status(400).json({ message: 'Adresse e-mail ou mot de passe manquant' });
+        }
+
+        req.email = email;
+        req.password = password;
+        next();
+    },
+
+    checkEmailPayload: (req, res, next) => {
+        const { email } = req;
+
+        if (!mailPattern.test(email)) {
+            return res.status(400).json({ message: 'Format d\'e-mail invalide' });
+        }
+
+        next();
+    },
+
+    checkPasswordPayload: (req, res, next) => {
+        const { password } = req;
+
+        if (!passwordPattern.test(password)) {
+            const errors = [];
+
+            if (!/(?=.*?[A-Z])/.test(password)) {
+                errors.push("Le mot de passe doit contenir au moins une lettre majuscule.");
+            }
+            if (!/(?=.*?[a-z])/.test(password)) {
+                errors.push("Le mot de passe doit contenir au moins une lettre minuscule.");
+            }
+            if (!/(?=.*?[0-9])/.test(password)) {
+                errors.push("Le mot de passe doit contenir au moins un chiffre.");
+            }
+            if (!/(?=.*?[#?!@$%^&*-])/.test(password)) {
+                errors.push("Le mot de passe doit contenir au moins un caractère spécial.");
+            }
+            if (password.length < 8) {
+                errors.push("Le mot de passe doit avoir au moins 8 caractères.");
+            }
+
+            return res.status(400).json({ message: "Votre mot de passe n'est pas conforme", errors });
+        }
+
+        next();
+    },
+
+    signupResponse: async (req, res) => {
+        try {
+            const { email, password } = req;
+    
+            // Vérifie si l'utilisateur existe déjà
+            const existingUser = await User.findOne({ email });
+    
+            if (existingUser) {
+                return res.status(400).json({ message: 'Email ' });
+            }
+    
+            // Si l'utilisateur n'existe pas, enregistre le nouvel utilisateur
+            const newUser = new User({
+                email,
+                ingredients: [],
+            });
+    
+            newUser.setPassword(password);
+    
+            const savedUser = await newUser.save();
+    
+            if (!savedUser) {
+                return res.status(500).json({ message: "Erreur lors de l'enregistrement de l'utilisateur." });
+            }
+    
+            // const token = savedUser.generateJWT();
+            res.status(201).json({ message: 'Utilisateur enregistré avec succès.'});
+        } catch (error) {
+            let status = 500;
+            let message = 'Erreur inattendue.';
+            if (error.message === 'Utilisateur déjà existant.') status = 400;
+    
+            console.error(new Date().toISOString(), 'controllers/admin-controllers.js > signupResponse > error ', error);
+    
+            return res.status(status).json({ message });
+        }
+    },
+    signupResponse: async (req, res) => {
+      
+    },
+
+
+};
---
+++ model/user.js
@@ -0,0 +1,55 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const crypto = require('crypto');
+const jwt = require('jsonwebtoken');
+
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      unique: true,
+      required: [true, 'Please add an email'],
+    },
+    ingredients: {
+      type: [String],
+    },
+    salt: {
+      type: String,
+    },
+    hash: {
+      type: String,
+    },
+  },
+  { collection: 'users' }
+);
+
+userSchema.methods.setPassword = function (password) {
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+    .toString('hex');
+};
+
+userSchema.methods.passwordIsValid = function (password) {
+  const generateHash = crypto
+    .pbkdf2Sync(password, this.salt, 1000, 64, 'sha512')
+    .toString('hex');
+
+  return this.hash === generateHash;
+};
+
+userSchema.methods.generateJWT = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      expiresIn: '1h'
+    },
+    process.env.JWT_SECRET || 'your_default_secret'
+  );
+
+
+};
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
